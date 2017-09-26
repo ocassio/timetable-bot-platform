be943da16dd91d8d159726e985e475b0891b28bb
ocassio <orlicus@gmail.com>
2017-09-26 20:38:51 +0400

    Timetable action fix

--- src/actions/timetable.action.js
+++ src/actions/timetable.action.js
@@ -1,4 +1,4 @@
-const { TIMETABLE_ACTION } = require('../consts/actions.consts')
+const { TIMETABLE_ACTION, MAIN_MENU_ACTION } = require('../consts/actions.consts')
 const { LAST_ACTION } = require('../consts/session.consts')
 const SessionService = require('../services/session.service')
 const PreferencesService = require('../services/preferences.service')
@@ -16,19 +16,22 @@ const timetableAction = {
         const criterion = await PreferencesService.getCriterion(userId)
         const timetable = await APIService.getTimetable(criterion.type, criterion.id, dateRange.from, dateRange.to)
         const messages = timetable.length > 0 ?
-            timetable.map(day => getDayMessage(day)) :
+            timetable.map(day => this.getDayMessage(day)) :
             ['Пары отсутствуют']
 
         return {
             response: {
                 messages: messages
+            },
+            next: {
+                action: MAIN_MENU_ACTION
             }
         }
     },
 
     getDayMessage(day) {
         let message = `${day.dayOfWeek} - ${day.date}\n`
-        day.lessons.forEach(lesson => message += '\n' + getLessonMessage(lesson))
+        day.lessons.forEach(lesson => message += '\n' + this.getLessonMessage(lesson))
 
         return message;
     },
@@ -36,11 +39,11 @@ const timetableAction = {
     getLessonMessage(lesson) {
         let message = `${lesson.number} пара | ${lesson.time.from} - ${lesson.time.to}\n`
         message += `${lesson.name}\n`
-        message += `Преподаватель: ${getValue(lesson.teacher)}\n`
-        message += `Аудитория: ${getValue(lesson.room)}\n`
-        message += `Вид занятия: ${getValue(lesson.type)}\n`
-        message += `Группы: ${getValue(lesson.group)}\n`
-        message += `Примечание: ${getValue(lesson.note)}\n`
+        message += `Преподаватель: ${this.getValue(lesson.teacher)}\n`
+        message += `Аудитория: ${this.getValue(lesson.room)}\n`
+        message += `Вид занятия: ${this.getValue(lesson.type)}\n`
+        message += `Группы: ${this.getValue(lesson.group)}\n`
+        message += `Примечание: ${this.getValue(lesson.note)}\n`
 
         return message;
     },
