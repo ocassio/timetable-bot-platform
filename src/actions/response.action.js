cec10021fb99e022c9641bfd0e07b073a95d75ca
ocassio <orlicus@gmail.com>
2017-09-30 14:24:23 +0400

    Handling non-format messages

--- src/actions/response.action.js
+++ src/actions/response.action.js
@@ -1,5 +1,5 @@
 const { LAST_ACTION } = require('../consts/session.consts')
-const { RESPONSE_ACTION } = require('../consts/actions.consts')
+const { RESPONSE_ACTION, MAIN_MENU_ACTION } = require('../consts/actions.consts')
 const SessionService = require('../services/session.service')
 
 const responseAction = {
@@ -9,12 +9,18 @@ const responseAction = {
 
     execute(params) {
         const lastAction = SessionService.getParam(params.userId, LAST_ACTION)
-        const action = require('./index').find(action => action.name === lastAction.action)
-        const mergedParams = Object.assign({}, params, lastAction)
-        if (action && action.handleResponse) {
-            return action.handleResponse(mergedParams)
+        if (lastAction) {
+            const action = require('./index').find(action => action.name === lastAction.action)
+            const mergedParams = Object.assign({}, params, lastAction)
+            if (action && action.handleResponse) {
+                return action.handleResponse(mergedParams)
+            }
         }
-    }
+
+        return {
+            next: { action: MAIN_MENU_ACTION }
+        }
+    },
 }
 
 module.exports = responseAction
